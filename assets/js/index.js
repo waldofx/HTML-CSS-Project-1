9da47dc3dabb041aa277341c321bf75f866c38d0
waldofx <90760767+waldofx@users.noreply.github.com>
2021-10-29 17:46:54 +0700

    Review Task of Fundamentals

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// function validateForm() {
+//     let fn = document.getElementById("fullname").value;
+//     if (fn == "") {
+//         document.getElementById("fullname").style.borderColor = "red";
+//         return false;
+//     } else {
+//         document.getElementById("fullname").style.borderColor = "green";
+//     }
+// }
