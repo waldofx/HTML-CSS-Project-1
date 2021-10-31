da528d45cc2e34f6d523e1f73cf7ce7bbcc5774f
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-01 03:10:28 +0700

    local storage + review_message css
    local storage + review_message css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,3 @@
-// function validateForm() {
-//     let fn = document.getElementById("fullname").value;
-//     if (fn == "") {
-//         document.getElementById("fullname").style.borderColor = "red";
-//         return false;
-//     } else {
-//         document.getElementById("fullname").style.borderColor = "green";
-//     }
-// }
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     "use strict";
